0a3d80215c4fe360b79b7d52c4f39c893c58474b
xu <1027639251@qq.com>
2018-07-06 16:10:58 +0800

    888

--- html/templated/liebiao.js
+++ html/templated/liebiao.js
@@ -8,7 +8,10 @@ template: `
 					<!--我的楼盘特有的封盘按钮-->
 				 <p v-if="name=='wodeloupan'"  @click.stop="caozuos(item.objectId,item,item.status,index)" class="border-b relative fz25 c555555 center" style="top:-0.2rem;height:0.6rem;line-height:0.6rem;">
 				 	<span class="czjz fz25" style="left:0;" :class="{cff0000:bb=item.status=='-1',cff4d00:item.status=='0',c999999:item.status=='-2',c36c748:item.status=='1'}">
-				 	{{item.status=='-1'?'不通過':(item.status=='0'?'審核中':(item.status=='1'?'已發佈':(item.status=='-2'?'已封盘':'')))}}
+           <!-- <p>{{item.status=='-1'?'不通過':(item.status=='0'?'審核中':(item.status=='1'?'已發佈':(item.status=='-2'?'已封盘':'')))}}</p>-->
+           {{item.show==0?'已封盤':'未封盤'}}
+           {{item.status==-1?'不通過':(item.status==0?'待審核':'已通過')}}
+
 				 	</span>
 				 	<img  src="../../assets/img/me/caozuo.png" alt="" style="width:0.29rem;right:0.6rem;" class="czjz" />
 				 	<span  class="czjz fz25 c000000" style="right:0;">操作</span>
